43c65c5caa0148f354b5d5cf1919677ea0b778ea
Vlad Osipov <vladyslav.osip@gmail.com>
2024-09-25 15:11:47 +0300

    add some file

---
+++ 1-gallery.js
@@ -0,0 +1,12 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as o}from"./assets/vendor-CgTBfC_f.js";const e=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",original:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",description:"Flower Blooms"},{preview:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",original:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",description:"Alpine Mountains"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",description:"Mountain Lake Sailing"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",description:"Alpine Spring Meadows"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",description:"Nature Landscape"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",description:"Lighthouse Coast Sea"}],p=document.querySelector(".gallery");p.style.padding="10px";p.style.display="flex";p.style.justifyContent="center";p.style.flexWrap="wrap";p.style.rowGap="24px";p.style.gap="24px";const n=e.map(({preview:t,original:i,description:a})=>`<li class="gallery-item" style="list-style: none;">
+  <a class="gallery-link" href="${i}">
+    <img
+      class="gallery-image"
+      src="${t}"
+      alt="${a}"
+      style="height: 200px; width: 360px;"
+    />
+  </a>
+</li>
+`).join("");p.innerHTML=n;new o(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
